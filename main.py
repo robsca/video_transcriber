e8207bf4cbdbc4906c7623e49bc8cec575529cc5
robsca <66328669+robsca@users.noreply.github.com>
2022-06-18 17:13:34 +0100

    Minor ui key error

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import ssl
 import os
 from moviepy.editor import VideoFileClip
 from brain import *
+import traceback
+# import sys
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -15,42 +17,43 @@ video_file = st.sidebar.file_uploader("Choose a video file", type=["mp4", "mov"]
 # get length of video with moviepy
 
 url = st.sidebar.text_input('Insert URL')
-txt = st.text_area('Transcript')
-if url:
-    summary, context = summarizer_from_url(url)
-
-elif txt:
-    summary, context = summarizer(txt)
-    st.text_area('Summary', summary)
-
-elif video_file is not None:
-    # save file
-    with open("video.mp4", "wb") as f:
-        f.write(video_file.read())
-    # clip
-    clip = VideoFileClip("video.mp4")
-    duration = clip.duration
-    print(duration)
-    st.sidebar.video(video_file)
-    time_per_second = (13*60) / 280
-    # run video to text
-    with st.spinner(f'Wait for it... it will take {(duration*time_per_second)//60} minuts'):
-        transcript = Video_to_Text('video.mp4').transcript
-        st.text_area('Transcript', transcript, height=500)
-        st.success('Done!')
-        summary, context = summarizer(transcript)
-else:
-    st.write('No file selected')
-
+txt = st.text_area('Transcript', key = 'transcript_')
 try:
-    st.text_area('Summary', summary)
+    if url:
+        summary, context = summarizer_from_url(url)
+
+    elif txt:
+        summary, context = summarizer(txt)
+        st.text_area('Summary', summary, key = 'summary')
+
+    elif video_file is not None:
+        # save file
+        with open("video.mp4", "wb") as f:
+            f.write(video_file.read())
+        # clip
+        clip = VideoFileClip("video.mp4")
+        duration = clip.duration
+        print(duration)
+        st.sidebar.video(video_file)
+        time_per_second = (13*60) / 280
+        # run video to text
+        with st.spinner(f'Wait for it... it will take {(duration*time_per_second)//60} minuts'):
+            transcript = Video_to_Text('video.mp4').transcript
+            st.text_area('Transcript', transcript, height=500,key= 'transcript')
+            st.success('Done!')
+            summary, context = summarizer(transcript)
+    else:
+        st.write('No file selected')
+
     with st.expander('Ask me a question'):
         question = st.text_input('Question')
         if question:
             answer = question_answering(summary, question)
-            st.text_area('Answer', answer['answer'])
+            st.text_area('Answer', answer['answer'], key='answer')  
 except:
     st.write('Problem')
+    # write which error occured
+    st.write(traceback.format_exc())
 
 if '__main__' == __name__:
     # run only once
